36e0c4fe3c185330a8250fca10b25509490d1671
liqianlong <liqianlong@heiniubao.com>
2020-07-24 23:26:46 +0800

    提交云村评论 （做的垃圾）

--- neteaseCloudMusicVue/src/pages/app/community/index/index.vue
+++ neteaseCloudMusicVue/src/pages/app/community/index/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
+  <div class="community-container">
+    <h2>云村热评</h2>
+    <div class="community-content" v-if="communityStatus">
+      <div class="community-item" v-for="(item, index) in communityList" :key="index">
+        <img :src="item.simpleResourceInfo.songCoverUrl">
+        <p>{{ item.content.substring(0, 18) }}...</p>
+      </div>
+    </div>
     <buttonNav></buttonNav>
   </div>
 </template>
@@ -17,7 +24,8 @@
     name: "communityIndex",
     data() {
       return {
-
+        communityStatus: false,
+        communityList: ''
       }
     },
     mounted() {
@@ -27,12 +35,13 @@
     methods: {
       getHotWallListFunc() {
         getHotWallList().then(res => {
-          console.log(res)
+          this.communityList = res.data
+          this.communityStatus = true
         })
       },
       getHotWallTopicFunc() {
         getHotTopicList().then(res => {
-          console.log(res)
+
         })
       }
     }
@@ -40,5 +49,35 @@
 </script>
 
 <style scoped>
-
+  .community-container h2 {
+    padding-top: .3rem;
+    margin-left: .3rem;
+    color: #333;
+    font-size: .3rem;
+    margin-bottom: .3rem;
+  }
+  .community-content {
+    width: 6.9rem;
+    margin: 0 auto;
+  }
+  .community-item {
+    position: relative;
+    width: 3rem;
+    display: inline-block;
+    margin-right: .1rem;
+  }
+  .community-item:nth-child(2n) {
+    margin-right: 0;
+    float: right;
+  }
+  .community-item img {
+    width: 100%;
+    display: block;
+    border-radius: .2rem;
+  }
+  .community-item p {
+    font-size: .3rem;
+    margin-top: .2rem;
+    color: #000;
+  }
 </style>
